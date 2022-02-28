1de404d8d68b2745bbe120727213e1870dee81ba
CliffRobinson <clifford.f.robinson@gmail.com>
2022-02-28 13:09:08 +1300

    very WIP state for wordle

--- wordle.exs
+++ wordle.exs
@@ -1,4 +1,45 @@
 defmodule Wordle do
+  def start_store do
+    spawn(fn ->
+      store_loop(%{
+        "a" => true,
+        "b" => true,
+        "c" => true,
+        "d" => true,
+        "e" => true,
+        "f" => true,
+        "g" => true,
+        "h" => true,
+        "i" => true,
+        "j" => true,
+        "k" => true,
+        "l" => true,
+        "m" => true,
+        "n" => true,
+        "o" => true,
+        "p" => true,
+        "q" => true,
+        "r" => true,
+        "s" => true,
+        "t" => true,
+        "u" => true,
+        "v" => true,
+        "w" => true,
+        "x" => true,
+        "y" => true,
+        "z" => true
+      })
+    end
+    )
+  end
+
+  def store_loop(store) do
+    receive do
+      {:remove, letter} ->
+        store_loop(%{store | })
+    end
+  end
+
   def play do
     select_answer()
     |> make_guess(6)
