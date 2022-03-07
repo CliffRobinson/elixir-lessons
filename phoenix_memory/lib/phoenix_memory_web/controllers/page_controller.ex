0559226f5341b862dc4258fc392d6ecb2c6c6ba6
CliffRobinson <clifford.f.robinson@gmail.com>
2022-03-08 10:42:17 +1300

    remove comments, comment out one more

--- phoenix_memory/lib/phoenix_memory_web/controllers/page_controller.ex
+++ phoenix_memory/lib/phoenix_memory_web/controllers/page_controller.ex
@@ -2,21 +2,18 @@ defmodule PhoenixMemoryWeb.PageController do
   use PhoenixMemoryWeb, :controller
 
   def index(conn, _params) do
-    # done_thing = PhoenixMemory.MemoryServer.do_a_thing()
-
-    %{:board => board, :guesses => guesses} = PhoenixMemory.MemoryServer.get_state()
+   %{:board => board, :guesses => guesses} = PhoenixMemory.MemoryServer.get_state()
 
     conn
     |> assign(:guesses, guesses)
     |> assign(:board, Enum.chunk_every(board,5))
-    # |> assign(:done_thing, done_thing)
     |> render("index.html")
   end
 
-  def post_thing(conn, %{ "thing" => thing }) do
-    PhoenixMemory.MemoryServer.get_a_thing(thing)
-    redirect(conn, to: "/")
-  end
+  # def post_thing(conn, %{ "thing" => thing }) do
+  #   PhoenixMemory.MemoryServer.get_a_thing(thing)
+  #   redirect(conn, to: "/")
+  # end
 
   def post_guess(conn, arg) do
     PhoenixMemory.MemoryServer.post_guess(arg)
