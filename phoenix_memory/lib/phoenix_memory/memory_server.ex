7f69f26955be6719ead0fc4c4d51416a5829c279
CliffRobinson <clifford.f.robinson@gmail.com>
2022-03-08 12:45:16 +1300

    working bar correctly determining win

--- phoenix_memory/lib/phoenix_memory/memory_server.ex
+++ phoenix_memory/lib/phoenix_memory/memory_server.ex
@@ -25,21 +25,19 @@ defmodule PhoenixMemory.MemoryServer do
     {:reply, state, state}
   end
 
-  def handle_call({:post_guess, stringGuessA, stringGuessB}, _from, state) do
+  def handle_call({:post_guess, string_first_index, string_second_index}, _from, state) do
     %{:board => board} = state
-    {guessA, _} = Integer.parse(stringGuessA)
-    {guessB, _} = Integer.parse(stringGuessB)
+    {first_index, _} = Integer.parse(string_first_index)
+    {second_index, _} = Integer.parse(string_second_index)
 
-    first_letter = get_letter(guessA, board)
-    second_letter = get_letter(guessB, board)
+    first_letter = get_letter(first_index, board)
+    second_letter = get_letter(second_index, board)
     match = first_letter == second_letter
 
-    check_guess(match, guessA, guessB, state)
+    check_guess(match, {first_index, first_letter, second_index, second_letter}, state)
   end
 
   defp get_letter(guess_index, state) do
-    IO.inspect("guess index is:")
-    IO.inspect(guess_index)
     {_index, _bool, found_letter} =
       Enum.find(state, fn {index, _bool, _letter} ->
         index == guess_index
@@ -48,21 +46,34 @@ defmodule PhoenixMemory.MemoryServer do
     found_letter
   end
 
-  def check_guess(true, first_index, second_index, %{:board => board, :guesses => guesses}) do
+  def check_guess(true, {first_index, first_letter, second_index, second_letter}, %{:board => board, :guesses => guesses}) do
     new_board =
       Enum.map(board, fn {index, bool, letter} ->
         {index, index == first_index || index == second_index || bool, letter}
       end)
 
     new_state = %{:board => new_board, :guesses => (guesses - 1) }
-    {:reply, :fail, new_state}
+
+    if (game_won?(new_board)) do
+      {:reply, {:success, {first_index, first_letter, second_index, second_letter}}, new_state}
+    else
+      {:reply, {:victory, {first_index, first_letter, second_index, second_letter}}, new_state}
+    end
+
+
   end
 
-  def check_guess(false, _first_index, _second_index, %{:board => board, :guesses => guesses}) do
+  def check_guess(false, {first_index, first_letter, second_index, second_letter}, %{:board => board, :guesses => guesses}) do
 
     new_state = %{:board => board, :guesses => (guesses - 1) }
 
-    {:reply,:success, new_state}
+    {:reply,{:fail, {first_index, first_letter, second_index, second_letter}}, new_state}
+  end
+
+  defp game_won?(board) do
+    Enum.reduce(board, true, fn ({_index, bool, _letter}, result) ->
+      if bool, do: result, else: false
+    end )
   end
 
   defp init_state() do
--- phoenix_memory/lib/phoenix_memory_web/controllers/page_controller.ex
+++ phoenix_memory/lib/phoenix_memory_web/controllers/page_controller.ex
@@ -1,18 +1,37 @@
 defmodule PhoenixMemoryWeb.PageController do
   use PhoenixMemoryWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, params) do
    %{:board => board, :guesses => guesses} = PhoenixMemory.MemoryServer.get_state()
 
+    # message = "initial defaul"
+
+    IO.inspect(params)
+
+    message = if(Map.get(params, "message"), do: Map.get(params, "message"), else: "Make your first guess!")
+
     conn
+    |> assign(:message, message)
     |> assign(:guesses, guesses)
     |> assign(:board, Enum.chunk_every(board,5))
     |> render("index.html")
   end
 
+  @spec post_guess(Plug.Conn.t(), map) :: Plug.Conn.t()
   def post_guess(conn, arg) do
-    PhoenixMemory.MemoryServer.post_guess(arg)
+    IO.puts("arg to post_guess")
+    IO.inspect(arg)
+    {guess_result, {first_index, first_letter, second_index, second_letter}} = PhoenixMemory.MemoryServer.post_guess(arg)
 
-    redirect(conn, to: "/")
+    message = cond do
+      guess_result == :victory ->
+        "You win! #{first_index} is #{first_letter} and #{second_index} is #{second_letter}"
+      guess_result == :success ->
+        "Congratulations, they matched! #{first_index} is #{first_letter} and #{second_index} is #{second_letter}"
+        guess_result == :fail ->
+          "Sorry, no match! #{first_index} is #{first_letter} and #{second_index} is #{second_letter}"
+    end
+
+    redirect(conn, to: "/?message=#{message}")
   end
 end
